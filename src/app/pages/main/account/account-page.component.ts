efd5ad10d88ed8ebbf0e31712eebbd0904abb005
DmitryR <dmytro.ryabchun@gmail.com>
2024-02-27 19:13:11 +0200

    🔨 Minor fixes

--- src/app/pages/main/account/account-page.component.ts
+++ src/app/pages/main/account/account-page.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'tvt-account-page',
   templateUrl: './account-page.component.html',
+  standalone: true,
+  imports: [
+    RouterOutlet
+  ],
   styleUrls: ['./account-page.component.scss']
 })
 export class AccountPageComponent {
--- src/app/pages/main/calculation/calculation-page.component.ts
+++ src/app/pages/main/calculation/calculation-page.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'tvt-calculation-page',
   templateUrl: './calculation-page.component.html',
+  standalone: true,
+  imports: [
+    RouterOutlet
+  ],
   styleUrls: ['./calculation-page.component.scss']
 })
 export class CalculationPageComponent {
