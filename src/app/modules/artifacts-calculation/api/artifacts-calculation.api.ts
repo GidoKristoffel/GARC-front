ab9fd5a41845b6c780aa237ca85a2bbfeedc4e21
DmitryR <dmytro.ryabchun@gmail.com>
2024-03-04 19:32:55 +0200

    🚧 Updated artifacts calculation api

--- src/app/modules/artifacts-calculation/api/artifacts-calculation.api.ts
+++ src/app/modules/artifacts-calculation/api/artifacts-calculation.api.ts
@@ -1,8 +1,23 @@
 import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { IAvailableCharactersResponse } from "../../available-characters/interfaces/api.interface";
+import { apiOptions } from "../../../core/constants/api.constant";
+import { environment } from "../../../../environment/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArtifactsCalculationApi {
-  constructor() {}
+  private readonly api: string = environment.api + '/client/calculation/artifacts';
+
+  constructor(private http: HttpClient) {}
+
+  public get(): Observable<IArtifactsCalculationResponse> {
+    return this.http.get<IArtifactsCalculationResponse>(this.api, apiOptions);
+  }
+
+  public update(data: IUpdateArtifactsCalculation): Observable<IArtifactsCalculationResponse> {
+    return this.http.post<IArtifactsCalculationResponse>(this.api, data, apiOptions);
+  }
 }
