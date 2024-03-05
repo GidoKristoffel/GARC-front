462be9e46b2a5ce807b4310b06a5f1c15c70e086
DmitryR <dmytro.ryabchun@gmail.com>
2024-03-05 18:04:44 +0200

    🚧 Updated artifacts calculation enums

---
+++ src/app/modules/artifacts-calculation/enums/artifact-leveling-categories.enum.ts
@@ -0,0 +1,7 @@
+export enum EArtifactLevelingCategories {
+  Assembled = 'assembled',
+  InAssembly = 'in-assembly',
+  ReadyForAssembly = 'ready-for-assemble',
+  PartiallyReadyForAssembly = 'partially-ready-for-assembly',
+  NotReadyForAssembly = 'not-ready-for-assembly',
+}
